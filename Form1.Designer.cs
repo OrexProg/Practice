56ce0fc7e9cdbad692e65669c54966cb6bee102d
Andrey <70953623+OrexProg@users.noreply.github.com>
2021-09-28 12:02:24 +0500

    Create

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnTest.Location = new System.Drawing.Point(362, 318);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(12, 45);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(776, 101);
+            this.tbInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(364, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(364, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(12, 189);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(776, 101);
+            this.tbOutput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 357);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.btnTest);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.MaximumSize = new System.Drawing.Size(821, 396);
+            this.MinimumSize = new System.Drawing.Size(821, 396);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbOutput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tbInput.Text = "How can mirrors be real if our eyes aren't real";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 7 - 7 % 2;
+            string testText = tbInput.Text;
+
+            tbOutput.Text = ToJadenCase(testText);
+        }
+        /// <summary>
+        /// Преобразование слов в верблюжий стиль
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToCamelCase(string str)
+        {
+            var result = str.Split('-','_');
+            string t = result[0];
+            foreach (string v in result.Skip(1))
+            {
+                t += v[0].ToString().ToUpper() + v.Substring(1, v.Length - 1);
+            }
+
+            str = t;
+            return str;
+        }
+        /// <summary>
+        /// Преобразование всех слов в предложениях в большие буквы
+        /// </summary>
+        /// <param name="phrase"></param>
+        /// <returns></returns>
+        public static string ToJadenCase(string phrase)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase);
+            //или
+            //return String.Join(" ", phrase.Split().Select(i => Char.ToUpper(i[0]) + i.Substring(1)));
+        }
+    }
+}
