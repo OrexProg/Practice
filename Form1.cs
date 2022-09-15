2d9472fd4af10ed5d46af3943faf4e0c8ebd7baf
Andrey <no>
2022-09-15 15:49:47 +0500

    +

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Globalization;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 
 namespace Practice
 {
@@ -16,7 +17,7 @@ namespace Practice
         public Form1()
         {
             InitializeComponent();
-            tbInput.Text = "indivisibility";
+            tbInput.Text = "...---... --..-- ...---... --..--";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -24,8 +25,7 @@ namespace Practice
             tbOutput.Clear();
 
             string testText = tbInput.Text;
-            var result = DuplicateCount(testText);
-            tbOutput.Text = result.ToString();
+            tbOutput.Text = null;
         }
         /// <summary>
         /// Преобразование слов в верблюжий стиль
@@ -325,6 +325,254 @@ namespace Practice
         {
             return str.ToLower().GroupBy(x => x).Where(x => x.Count() > 1).Count();
         }
+        /// <summary>
+        /// Расшифровка римских цифр
+        /// </summary>
+        /// <param name="roman"></param>
+        /// <returns></returns>
+        public static int Solution(string roman)
+        {
+            Dictionary<char, int> rome_dict = new Dictionary<char, int>() { { 'I', 1 },{'V',5},{'X',10},{'L',50},{'C',100},{'D',500}, { 'M', 1000 } };
 
+            int result = 0;
+            int max = 0;
+
+            foreach (var c in roman.Reverse())
+            {
+                int value = rome_dict[c];
+
+                if (value < max)
+                {
+                    result -= value;
+                }
+                else
+                {
+                    result += value;
+                    max = value;
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Вывод кол-ва букв в слове 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>Выводим списком</returns>
+        public static Dictionary<char, int> Count(string str)
+        {
+            Dictionary<char, int> countLetter = new Dictionary<char, int>();
+            var test = str.ToLower().Select(x => x);
+            str.ToLower().GroupBy(x => x).ToList().ForEach(x => countLetter.Add(x.Key, test.Count(y => y == x.Key)));
+            
+            return countLetter;
+
+            //Лучший вариант
+            //return str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+        }
+        /// <summary>
+        /// Вывод порядковых номеров сумма которых дает target
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < numbers.Count(); i++)
+            {
+                for(int j = 1; j < numbers.Count(); j++)
+                {
+                    if ( i != j && (numbers[i] + numbers[j] == target))
+                    {
+                        result.Add(i);
+                        result.Add(j);
+
+                        return result.ToArray();
+                    }
+                }
+            }
+            return new int[0];
+        }
+        //Супер умный вариант public static int[] TwoSum(int[] numbers, int target) => numbers.Select((x,i)=>new [] {i, Array.IndexOf(numbers,target-x,i+1)}).First(x => x[1] != -1);
+        /// <summary>
+        /// Замена букв на 13 после текущей буквы в алфавите
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static string Rot13(string message)
+        {
+            List<char> message_arr = message.ToList<char>();
+            List<char> result_arr = new List<char>();
+            string result = null;
+            message_arr.ForEach(x =>
+            {
+                if (x > 64 && x < 91)
+                {
+                    result_arr.Add(Convert.ToChar((x + 13) > 90 ? 64 + (x + 13 - 90) : x + 13));
+                }
+                else if (x > 96 && x < 123)
+                {
+                    result_arr.Add(Convert.ToChar((x + 13) > 122 ? 96 + (x + 13 - 122) : x + 13));
+                }
+                else
+                {
+                    result_arr.Add(x);
+                }
+            });
+
+            result_arr.ForEach(x =>
+            {
+                result += Convert.ToChar(x);
+            });
+
+            return result;
+
+
+            /*Лучший вариант
+             
+             string result = "";
+            foreach (var s in message)
+            {
+                if ((s >= 'a' && s <= 'm') || (s >= 'A' && s <= 'M'))
+                    result += Convert.ToChar((s + 13)).ToString();
+                else if ((s >= 'n' && s <= 'z') || (s >= 'N' && s <= 'Z'))
+                    result += Convert.ToChar((s - 13)).ToString();
+                else result += s;
+            }
+            return result;
+             
+            // Самый лучший вариант
+            return String.Join("", message.Select(x => char.IsLetter(x) ? (x >= 65 && x <= 77) || (x >= 97 && x <= 109) ? (char)(x + 13) : (char)(x - 13) : x));
+
+             */
+        }
+        /// <summary>
+        /// Расшифровка азбуки морзе
+        /// </summary>
+        /// <param name="morseCode"></param>
+        /// <returns></returns>
+        public static string Decode(string morseCode)
+        {
+            Dictionary<string, string> morze_dict = new Dictionary<string, string>() {
+                { ".-", "A"}, { "-...", "B"},{ "-.-.", "C"},{ "-..", "D"},
+                { ".", "E"}, { "..-.", "F"}, { "--.", "G"}, { "....", "H"},
+                { "..", "I"}, { ".---", "J"}, { "-.-", "K"}, { ".-..", "L"},
+                { "--", "M"}, { "-.", "N"}, { "---", "O"}, { ".--.", "P"},
+                { "--.-", "Q"}, { ".-.", "R"}, { "...", "S"}, { "-", "T"},
+                { "..-", "U"}, { "...-", "V"}, { ".--", "W"}, { "-..-", "X"},
+                { "-.--", "Y"},{ "--..", "Z"},{"...---...","SOS"},{"--..--","!"},{"-.-.--","!"},{".-.-.-","."},
+            };
+            string result = null;
+
+            List<string> word = morseCode.Trim().Replace("  ", " 1 ").Split(' ').ToList();
+
+            word.ForEach(x =>
+            {
+                if (x != "1")
+                    result += morze_dict.GetValueOrDefault(x);
+                else
+                    result += " ";
+            });
+
+            return result.Trim();
+        }
+
+        /// <summary>
+        /// Сложение больших чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static string Add(string a, string b)
+        {
+            string res = "";
+            int c = 0, d = 0;
+
+            //выравниваем количество разрядов путем дополнения нулями слева
+            int max = Math.Max(a.Length, b.Length);
+            if (a.Length < b.Length)
+                a = a.PadLeft(max, '0');
+            else
+            if (a.Length > b.Length)
+                b = b.PadLeft(max, '0');
+
+            //складываем
+            for (int i = max - 1; i >= 0; i--)
+            {
+                c = (d + (int)Char.GetNumericValue(a[i]) + (int)Char.GetNumericValue(b[i])) % 10;
+                res += c.ToString();
+                d = (d + (int)Char.GetNumericValue(a[i]) + (int)Char.GetNumericValue(b[i])) / 10;
+            }
+            if (d != 0)
+                res += d;
+
+            return new String(res.Reverse().ToArray());
+        }
+        /// <summary>
+        /// Вывод диапозона чисел если больше 3 то через - если не подряд или 2 то через запятую
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string Extract(int[] args)
+        {
+            List<int> numbers = args.ToList();
+            numbers.Sort();
+
+            if(numbers.Count == 2)
+            {
+                return numbers[0] + "," + numbers[1];
+            }
+
+            string result = numbers[0].ToString();
+            int? max = null;
+            int count = 0;
+            bool isMax = false;
+            for(int i = 1;i<numbers.Count; i++)
+            {
+                var t = numbers[i];
+                if (numbers[i] <= 0)
+                {
+                    isMax = Math.Abs(numbers[i]) == Math.Abs(numbers[i - 1]) - 1;
+                }
+                else
+                {
+                    isMax = numbers[i] - 1 == numbers[i - 1];
+                }
+
+                if (isMax)
+                {
+                    max = numbers[i];
+                    count++;
+                }
+                else
+                {
+                    if (max == null)
+                        result += "," + numbers[i];
+                    else
+                    {
+                        if (count >1 && max != null)
+                        {
+                            result +=  "-" + max + "," + numbers[i];
+                            max = null;
+                        }
+                        else
+                        {
+                            result += max != null ?"," + max + "," + numbers[i] :"," + numbers[i];
+                            max = null;
+                        }
+                        
+                    }
+                    count = 0;
+                }
+            }
+
+            if(max != null && count > 1)
+                result +=  "-" + max;
+            else if(max!=null)
+                result += "," + max;
+
+            return result;  //TODO
+        }
     }
 }
